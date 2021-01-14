4029a89c64815b3cf284c27b7a38c6706d93b8de
sneh1212 <31516571+sneh1212@users.noreply.github.com>
2021-01-14 08:51:58 +0530

    React-jsx

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>My fav food</h1>
+    <ul>
+      <li> Roti</li>
+      <li> chawal </li>
+      <li> sawaji </li>
+    </ul>
+  </div>,
+
+  document.getElementById("root")
+);
+
 //Create a react app from scratch.
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
